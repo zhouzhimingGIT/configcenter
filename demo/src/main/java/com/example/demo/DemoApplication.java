f59297274d9049953713515b0e70c199aa92a65b
zhouzhimingGIT <45554797+zhouzhimingGIT@users.noreply.github.com>
2025-04-30 13:28:20 +0800

    init demo

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.jmx.support.RegistrationPolicy;
+
+@SpringBootApplication
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(DemoApplication.class, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
